c0f67d649a05b5972afd130e4a3e9574372c436e
sevda <imanysevda@gmail.com>
2021-05-14 16:01:52 +0430

    fixed a mistake

--- Individual.py
+++ Individual.py
@@ -1,16 +1,22 @@
 import random
-class Individual():
 
-    def __init__(self, chromosome , length):
+CHROMOSOME_LENGTH = 12
+
+class Individual:
+
+
+    def __init__(self, chromosome):
         self.chromosome = chromosome
         self.fitness = 0
-        self.length = length
-        
-    def randomMove(self):
+
+    @classmethod 
+    def randomMove(cls):
         gene = random.randint(0,2)
         return gene
 
-    def create_chromosome(self):
-        return [self.randomMove() for _ in range(self.length)]
+    @classmethod
+    def create_chromosome(cls):
+        global CHROMOSOME_LENGTH
+        return [cls.randomMove() for _ in range(CHROMOSOME_LENGTH)]
     
 
