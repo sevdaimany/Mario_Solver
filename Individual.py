39a148e2d96d487d819d8436f63b1245af8426d1
sevda <imanysevda@gmail.com>
2021-05-14 15:18:00 +0430

    add create_chromosome method

--- Individual.py
+++ Individual.py
@@ -1,6 +1,16 @@
+import random
 class Individual():
 
-    def __init__(self, chromosome):
+    def __init__(self, chromosome , length):
         self.chromosome = chromosome
         self.fitness = 0
+        self.length = length
         
+    def randomMove(self):
+        gene = random.randint(0,2)
+        return gene
+
+    def create_chromosome(self):
+        return [self.randomMove() for _ in range(self.length)]
+    
+
