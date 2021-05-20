3c6159faf5054830559be45ce3c4def3c8b9c1c1
sevda <imanysevda@gmail.com>
2021-05-20 11:40:47 +0430

    fixed crossover and change fitness methods

--- Individual.py
+++ Individual.py
@@ -18,16 +18,15 @@ class Individual:
         return gene
 
     @classmethod
-    def create_chromosome(cls):
-        
+    def create_chromosome(cls):  
         return [cls.randomMove() for _ in range(cls.CHROMOSOME_LENGTH)]
     
 
     def mutation(self):
         p = random.random()
-        if p < 0.2 :
-            randIndex = random.randint(0, self.CHROMOSOME_LENGTH -1) 
-            self.chromosome[randIndex] = 0
+        if p  < 0.2:
+             randIndex = random.randint(0, self.CHROMOSOME_LENGTH -1) 
+             self.chromosome[randIndex] = 0
     
 
     def crossover (self , mate):
@@ -41,27 +40,88 @@ class Individual:
 
             # if prob is less than 0.45, insert gene
             # from parent 1
-            if prob < 0.45:
+            if prob < 0.5:
             	child_chromosome.append(gp1)
 
             # if prob is between 0.45 and 0.90, insert
             # gene from parent 2
-            elif prob < 0.90:
+            else:
             	child_chromosome.append(gp2)
 
             # otherwise insert random gene(mutate),
             # for maintaining diversity
-            else:
-            	child_chromosome.append(self.randomMove())
+            
+            self.mutation()
 
         # create new Individual(offspring) using
         # generated chromosome for offspring
         return Individual(child_chromosome)
 
 
-    def fittness(self):
+    # def fittness(self):
 
+    #     score = 0
+    #     for i in range(self.CHROMOSOME_LENGTH):
+    #         current_step = self.level[i]
+
+    #         # for useless jump
+    #         if( self.chromosome[i] == 1 ):
+    #             if(i == self.CHROMOSOME_LENGTH - 2):
+    #                 if(self.level[i + 1] == "_" or self.level[i + 1] == "M"):
+    #                     score += -5
+    #             elif(i < self.CHROMOSOME_LENGTH - 2):
+    #                 if((self.level[i + 1] == "_" and self.level[i + 2] == "_") or (self.level[i + 1] == "M" and self.level[i + 2] == "M")
+    #                     or (self.level[i + 1] == "_" and self.level[i + 2] == "M") or (self.level[i + 1] == "M" and self.level[i + 2] == "_")):
+    #                     score += -5
+            
+    #         # restriction for double jump or continuous jumping and slipping continuous
+    #         if(self.chromosome[i] == 1 and i != (self.CHROMOSOME_LENGTH - 1) 
+    #             and self.chromosome[i + 1] != 0):
+    #                 score += -5
+
+    #                 continue
+
+
+    #         # main part
+           
+    #         if (current_step == 'G'):
+
+    #             if(self.chromosome[i - 1] == 1 and i == 1):
+    #                 score += 10
+    #             elif(self.chromosome[i - 2] == 1 and (i != 0 and i != 1)):
+    #                 score += 20
+    #             elif(self.chromosome[i - 1] == 1 and  (i != 0 and i != 1)):
+    #                 score += 10
+    #             else:
+    #                 score += -99999
+    #                 continue
+                
+    #         elif (current_step == 'L' ):
+
+    #             if( self.chromosome[i - 1] == 2 and i != 0):
+    #                 score += 10
+    #             else :
+    #                 score += -99999
+    #                 continue
+                
+    #         elif (current_step == 'M' and (i == 0 or self.chromosome[i - 1] != 1) ):
+    #                 score += 20
+
+            
+            
+
+    #     # for jump befor flag
+    #     if(self.chromosome[-1] == 1):
+    #         score += 10
+
+    #     return score
+    
+
+    def fittness(self):
+    
         score = 0
+        longestPath = 0
+        lenPath =0
         for i in range(self.CHROMOSOME_LENGTH):
             current_step = self.level[i]
 
@@ -69,51 +129,56 @@ class Individual:
             if( self.chromosome[i] == 1 ):
                 if(i == self.CHROMOSOME_LENGTH - 2):
                     if(self.level[i + 1] == "_" or self.level[i + 1] == "M"):
-                        score += -5
+                        score += -1
                 elif(i < self.CHROMOSOME_LENGTH - 2):
                     if((self.level[i + 1] == "_" and self.level[i + 2] == "_") or (self.level[i + 1] == "M" and self.level[i + 2] == "M")
                         or (self.level[i + 1] == "_" and self.level[i + 2] == "M") or (self.level[i + 1] == "M" and self.level[i + 2] == "_")):
-                        score += -5
-            
-            # restriction for double jump or continuous jumping and slipping continuous
-            if(self.chromosome[i] == 1 and i != (self.CHROMOSOME_LENGTH - 1) 
-                and self.chromosome[i + 1] != 0):
-                    score += -5
+                        score += -1
 
-                    continue
+            if( self.chromosome[i] == 2 ):
+               if i <= self.CHROMOSOME_LENGTH -2 and  self.level[i+1] != 'L' :
+                   score += -1
+               if i > self.CHROMOSOME_LENGTH -2:
+                    score += -1
 
-
-            # main part
-           
+        
+        
             if (current_step == 'G'):
+                if (self.chromosome[i-1] != 1 and i >= 1) or (self.chromosome[i-2] != 1 and i >= 2):
+                     if lenPath > longestPath :
+                        longestPath = lenPath
+                        lenPath = 0
+                if (self.chromosome[i-2] == 1 and i >=2 ):
+                    score +=2
 
-                if(self.chromosome[i - 1] == 1 and i == 1):
-                    score += 10
-                elif(self.chromosome[i - 2] == 1 and (i != 0 and i != 1)):
-                    score += 20
-                elif(self.chromosome[i - 1] == 1 and  (i != 0 and i != 1)):
-                    score += 10
-                else:
-                    score += -99999
-                    continue
                 
             elif (current_step == 'L' ):
 
-                if( self.chromosome[i - 1] == 2 and i != 0):
-                    score += 10
-                else :
-                    score += -99999
-                    continue
+                if(self.chromosome[i-1] != 2 and i >=1 ):
+                      if lenPath > longestPath :
+                        longestPath = lenPath
+                        lenPath = 0
+                if self.chromosome[i-2] == 1 and i >=2 :
+                      if lenPath > longestPath :
+                        longestPath = lenPath
+                        lenPath = 0
+               
                 
             elif (current_step == 'M' and (i == 0 or self.chromosome[i - 1] != 1) ):
-                    score += 20
+                    score += 2
+            lenPath += 1
 
             
-            
+        if lenPath > longestPath :
+            longestPath = lenPath
+            lenPath = 0
+        
+        score += longestPath
+                
 
         # for jump befor flag
         if(self.chromosome[-1] == 1):
-            score += 10
+            score += 1
 
         return score
 
