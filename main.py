a8f0e674a643dc6f206762a8b51e929a60181516
rajabi2001 <mj.rajabi2001@gmail.com>
2021-05-31 13:50:46 +0430

    Convergence AND Chart

--- main.py
+++ main.py
@@ -2,21 +2,37 @@ from Individual import Individual
 import random
 import json
 import eel
+import matplotlib.pyplot as plt
+import math
 
 POPULATION_SIZE = 200
-eel.init("frontend")
+# eel.init("frontend")
 
-@eel.expose
+# @eel.expose
 def main():
-    mylevel = "____G_ML__G_"
+
+
+    # address = "level6.txt"
+    # with open(address) as reader :
+    #     mylevel = reader.read()
+
+    mylevel = "_M_M_GM___LL__G__L__G_M__"
     generation = 1
     population = []
+    avgfit = []
     solve = False
     initial(POPULATION_SIZE , population , mylevel)
 
     population = sorted(population, key = lambda x:x.fitness)
-   
-    for i in range(20):
+
+    avgfit.append(0)
+    avg = 0
+    for i in population:
+        avg += i.fitness
+    avg = avg / len(population)
+    avgfit.append(avg)
+
+    while avgfit[-1] - avgfit[-2] > 0.0001 or avgfit[-1] < 0:
         new_generation = [] 
 
         s = int((10*POPULATION_SIZE)/100)
@@ -32,23 +48,49 @@ def main():
         	new_generation.append(child2)  
 
 
+        population = new_generation 
 
-        population = new_generation    
+        avg = 0
+        for i in population:
+            avg += i.fitness
+        avg = avg / len(population)
+        avgfit.append(avg)
+
+
         population = sorted(population, key = lambda x:x.fitness)
         [print("{} , {}, {}".format(i.chromosome , i.fitness , generation)) for i in population]
                
         generation += 1
+
     changed = changeAnswerForMap(population[-1].chromosome)
+
+    # for i in avgfit:
+    #     print(i , end=" ")
+
+    chart(range(generation),avgfit[1::])
+
     return get_json_result({
      "map" : list(mylevel),
       "answer" : changed,
     },
     )
 
+    
 
 
 
 
+def chart(x , y):
+    
+    plt.plot(x, y)
+    
+    plt.xlabel('fittnes')
+    
+    plt.ylabel('generation')
+    
+    plt.title('genetic algorithm ')
+    
+    plt.show()
 
 
 
@@ -93,7 +135,11 @@ def changeAnswerForMap(answer):
         else:
             checkContinue = True
     return changed
-    
 
 
-eel.start('index.html' ,size=(500,500))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
+# eel.start('index.html' ,size=(500,500))
